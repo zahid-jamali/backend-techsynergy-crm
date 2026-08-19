52a75f2ebb64d5b5cdd0275da9357e5f08b62318
Zahid Jamali <zahid.jamali4848@gmail.com>
2026-08-19 17:04:11 +0500

    no changes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const accountControllers = require('../controllers/accountControllers.js');
-const { verifyJWT, requireAdmin } = require('../lib/middleware.js');
+const express = require("express");
+const accountControllers = require("../controllers/accountControllers.js");
+const { verifyJWT, requireAdmin } = require("../lib/middleware.js");
 const router = express.Router();
 
-router.post('/create', verifyJWT, accountControllers.createAccount);
-router.get('/my', verifyJWT, accountControllers.getMyAccounts);
-router.put('/update/:id', verifyJWT, accountControllers.updateMyAccount);
-router.delete('/delete/:id', verifyJWT, accountControllers.deleteMyAccount);
+router.post("/create", verifyJWT, accountControllers.createAccount);
+router.get("/my", verifyJWT, accountControllers.getMyAccounts);
+router.put("/update/:id", verifyJWT, accountControllers.updateMyAccount);
+router.delete("/delete/:id", verifyJWT, accountControllers.deleteMyAccount);
 
 // Admin Routes
-router.get('/all', verifyJWT, requireAdmin, accountControllers.getAllAccounts);
+router.get("/all", verifyJWT, accountControllers.getAllAccounts);
 
 module.exports = router;
